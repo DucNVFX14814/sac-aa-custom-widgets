c585f94bedad6a0b2adf8de5698e0818da3c8d90
Nguyen Viet Duc <95999745+DucNVFX14814@users.noreply.github.com>
2023-05-22 14:58:18 +0700

    Add files via upload

---
+++ getScript.js
@@ -0,0 +1,40 @@
+let getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve);
+  });
+};
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `;
+
+  class GetScript extends HTMLElement {
+    constructor() {
+      super();
+
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+      this._root = this._shadowRoot.getElementById("root");
+      this.addEventListener("click", () => {
+        this.dispatchEvent(new Event("onClick"));
+      });
+    }
+
+    onCustomWidgetResize() {
+    }
+    // ------------------
+    // Scripting methods
+    // ------------------
+    async getScript(url) {
+      await getScriptPromisify(url);
+    }
+  }
+
+  customElements.define("com-sap-sample-getscript", GetScript);
+})();
